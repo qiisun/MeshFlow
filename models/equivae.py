77f54337e76a57549bc8746166b09f712db06db2
qsun1 <sq008@mail.ustc.edu.cn>
2025-12-01 13:36:11 +0800

    1

--- models/equivae.py
+++ models/equivae.py
@@ -3,10 +3,16 @@ sys.path.append('.')
 import math
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
 
 from models.utils import get_2d_sincos_pos_embed
 from models.equidit import LabelEmbedder, XEmbedder, VertexCrossAttention, DiTLayer, FinalLayer
 
+
+def float_to_index(value, min_val=-0.5, max_val=0.5, num_bins=512):
+    norm = (value - min_val) / (max_val - min_val)
+    norm = torch.clamp(norm, 0, 1)
+    return (norm * (num_bins - 1)).long()
 
 class AutoencoderKL(nn.Module):
     def __init__(self, 
@@ -108,12 +114,15 @@ class DiagonalGaussianDistribution(object):
 
 # modelify from equiDIT
 class Model(nn.Module):
-    def __init__(self, hidden_dim=768, num_heads=12, max_length=800, 
-                 input_dim=9, num_layers=6, gradient_checkpointing=True, 
+    def __init__(self, hidden_dim=768, num_heads=12, max_length=800,
+                 num_layers=6, gradient_checkpointing=True, 
                  class_dropout_prob=0.1, use_coord_encoding=True, 
                  version=3, pe_freq=20, mixed_precision='bf16',
                  use_dit_like_pe=False, face_cond=True, face_bin=10,
-                 use_rmsnorm=False, model_type='encoder'):
+                 use_rmsnorm=False, 
+                 model_type='encoder',
+                 decoder_type="reg",
+                 num_bins=512):
         super().__init__()
         self.model_type = model_type
         
@@ -153,7 +162,11 @@ class Model(nn.Module):
         self.vertex_cross_attn = VertexCrossAttention(
             modulation_type=modulation_type, hidden_dim=hidden_dim)
         if model_type == 'decoder':
-            self.final_layer = FinalLayer(hidden_dim, input_dim, use_rmsnorm=use_rmsnorm) # input_dim: 9 for v2, 3 for v3
+            if decoder_type == "cls":
+                self.num_bins = num_bins
+                self.final_layer = FinalLayer(hidden_dim, input_dim*num_bins, use_rmsnorm=use_rmsnorm) # input_dim: 9 for v2, 3 for v3
+            elif decoder_type == "reg":
+                self.final_layer = FinalLayer(hidden_dim, input_dim, use_rmsnorm=use_rmsnorm) # input_dim: 9 for v2, 3 for v3
         self.initialize_weights()
     
 
@@ -224,11 +237,14 @@ class Model(nn.Module):
         
         if self.model_type == 'encoder':
             return x # [b, N*3, C]
-        x = self.final_layer(x, c) # v2: [B, N*3, 3]
-        
-        if self.version > 1:
-            x = x.view(B, N, 9)
-        return x
+        elif self.model_type == 'decoder':
+            x = self.final_layer(x, c) # v2: [B, N*3, 3]
+            
+            if self.decoder_type == "cls":
+                x = x.view(B, N, 9, self.num_bins) # [B, N, 9, num_bins]
+            elif self.decoder_type == "reg":
+                x = x.view(B, N, 9)
+            return x
 
 def _masked_mean(x, mask):
     if mask is None: 
@@ -240,9 +256,17 @@ def _masked_mean(x, mask):
          mask = mask.repeat_interleave(scale, dim=1)
     return (x * mask).sum() / (mask.expand_as(x).sum() + 1e-8)
 
-def loss_vae(inputs, recon, posterior, mask=None, kl_weight=1e-6):
+def loss_vae(inputs, recon, posterior, mask=None, kl_weight=1e-6, decoder_type="reg"):
     # 1. 计算原始 diff
-    rec_diff = torch.abs(inputs - recon)
+    if decoder_type == "cls":
+        target_idx = float_to_index(recon, num_bins=512).to(recon.device)
+        rec_diff = F.cross_entropy(
+            inputs.permute(0, 3, 1, 2), 
+            target_idx, 
+            reduction='none'
+            )   
+    elif decoder_type == "reg":
+        rec_diff = torch.abs(inputs - recon)
     kl_diff = posterior.kl() # [B, N] or [B, N, C]
 
     # 2. 应用 Mask 并求平均
@@ -253,7 +277,8 @@ def loss_vae(inputs, recon, posterior, mask=None, kl_weight=1e-6):
     loss = rec_loss + kl_weight * kl_loss
     
     return loss, rec_loss, kl_loss
-    
+
+
 
 if __name__ == '__main__':    
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
